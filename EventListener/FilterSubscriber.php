13a9348df28703839629d02452d3fce6f16f78aa
polidog <polidogs@gmail.com>
2017-06-10 17:06:49 +0900

    Remove Controller property.

--- EventListener/FilterSubscriber.php
+++ EventListener/FilterSubscriber.php
@@ -28,11 +28,6 @@ class FilterSubscriber implements EventSubscriberInterface
     private $executor;
 
     /**
-     * @var object
-     */
-    private $controller;
-
-    /**
      * FilterSubscriber constructor.
      * @param Executor $executor
      */
@@ -45,24 +40,16 @@ class FilterSubscriber implements EventSubscriberInterface
     public function onKernelController(FilterControllerEvent $event)
     {
         $request = $event->getRequest();
-        $controller = $event->getController();
-
-        if (!is_array($controller) && method_exists($controller, '__invoke')) {
-            $controller = [$controller, '__invoke'];
-        } else if (!is_array($controller)) {
-            return;
-        }
 
         $classAnnotations = $request->attributes->get('_filter_class');
         $methodAnnotations = $request->attributes->get('_filter_method');
-        $this->controller = $controller[0];
 
         if ($classAnnotations !== null) {
-            $this->executeFilters(Filter::TYPE_BEFORE, $classAnnotations, $this->controller, $event);
+            $this->executeFilters(Filter::TYPE_BEFORE, $classAnnotations, $event);
         }
 
         if ($methodAnnotations !== null) {
-            $this->executeFilters(Filter::TYPE_BEFORE, $methodAnnotations, $this->controller, $event);
+            $this->executeFilters(Filter::TYPE_BEFORE, $methodAnnotations, $event);
         }
 
     }
@@ -75,10 +62,10 @@ class FilterSubscriber implements EventSubscriberInterface
         $methodAnnotations = $request->attributes->get('_filter_method');
 
         if ($classAnnotations !== null) {
-            $this->executeFilters(Filter::TYPE_AFTER, $classAnnotations, $this->controller, $event);
+            $this->executeFilters(Filter::TYPE_AFTER, $classAnnotations, $event);
         }
         if ($methodAnnotations !== null) {
-            $this->executeFilters(Filter::TYPE_AFTER, $methodAnnotations, $this->controller, $event);
+            $this->executeFilters(Filter::TYPE_AFTER, $methodAnnotations, $event);
         }
 
         return $event;
@@ -88,13 +75,12 @@ class FilterSubscriber implements EventSubscriberInterface
     /**
      * @param string $type
      * @param array $annotations
-     * @param object $controller controller object
      * @param KernelEvent $event
      */
-    private function executeFilters(string $type, array $annotations, $controller, KernelEvent $event)
+    private function executeFilters(string $type, array $annotations, KernelEvent $event)
     {
         foreach ($annotations as $annotation) {
-            $this->executeFilter($type, $annotation, $controller, $event);
+            $this->executeFilter($type, $annotation, $event);
         }
     }
 
@@ -104,7 +90,7 @@ class FilterSubscriber implements EventSubscriberInterface
      * @param object $controller controller object
      * @param KernelEvent $event
      */
-    private function executeFilter(string $type, FilterInterface $annotation, $controller, KernelEvent $event)
+    private function executeFilter(string $type, FilterInterface $annotation, KernelEvent $event)
     {
         if ($annotation->getType() === $type) {
             $this->executor->run($annotation, $event);
